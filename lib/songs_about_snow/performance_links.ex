e83d7d90f5ff68eeb83f40b72ddf1fde950fb0b8
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-09-04 13:07:31 -0400

    Add NMM compositions

--- lib/songs_about_snow/performance_links.ex
+++ lib/songs_about_snow/performance_links.ex
@@ -1,8 +1,12 @@
 defmodule SongsAboutSnow.PerformanceLinks do
   def links do
     [
-      {"Boum, for soprano voice, written for Stephanie Lamprea",
-       "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"}
+      {"Boum (2020), for soprano voice, written for Stephanie Lamprea",
+       "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"},
+      {"Amuse Bouche (2021), for alto saxophone, written for Stephan Tamas",
+       "https://youtu.be/bq7HA0YY9eM?si=VmwkdSDs1piBQCto&t=2307"},
+      {"a sensation of order (2022), for Eb clarinet, written for Raymond Brien",
+       "https://youtu.be/g-nqfkkmf3A?si=th5POhukLkCnkqMi&t=867"}
     ]
   end
 end
