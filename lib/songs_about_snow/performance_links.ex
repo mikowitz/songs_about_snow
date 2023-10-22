3c5f55ac0d54e2ec5a6f66223cc0e8da1e4e8afd
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-10-22 18:34:21 -0400

    Add Earth and Green

--- lib/songs_about_snow/performance_links.ex
+++ lib/songs_about_snow/performance_links.ex
@@ -1,12 +1,14 @@
 defmodule SongsAboutSnow.PerformanceLinks do
   def links do
     [
-      {"Boum (2020), for soprano voice, written for Stephanie Lamprea",
-       "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"},
+      {"Earth and Green (2019, rev. 2023), for piano solo",
+       "https://www.youtube.com/watch?v=njsQobL6Q_Y"},
+      {"a sensation of order (2022), for Eb clarinet, written for Raymond Brien",
+       "https://youtu.be/g-nqfkkmf3A?si=th5POhukLkCnkqMi&t=867"},
       {"Amuse Bouche (2021), for alto saxophone, written for Stephan Tamas",
        "https://youtu.be/bq7HA0YY9eM?si=VmwkdSDs1piBQCto&t=2307"},
-      {"a sensation of order (2022), for Eb clarinet, written for Raymond Brien",
-       "https://youtu.be/g-nqfkkmf3A?si=th5POhukLkCnkqMi&t=867"}
+      {"Boum (2020), for soprano voice, written for Stephanie Lamprea",
+       "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"}
     ]
   end
 end
