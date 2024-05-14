92abebff56ab0206f190bb1ba79da4d796d2de61
Michael berkowitz <michael.berkowitz@gmail.com>
2024-05-14 11:03:33 -0400

    Add recital Youtube links

--- lib/songs_about_snow/performance_links.ex
+++ lib/songs_about_snow/performance_links.ex
@@ -1,14 +1,22 @@
 defmodule SongsAboutSnow.PerformanceLinks do
   def links do
     [
+      {"Lorsque j'avais six ans (2023-2024), for cello and percussion",
+        "https://www.youtube.com/watch?v=aJwRZnvec54"},
+      {"Three Rothko Miniatures (2019, 2023-2024), for piano solo",
+        "https://www.youtube.com/watch?v=-I1hWFkU_es"},
+      {"Helen (2024)",
+        "https://www.youtube.com/watch?v=KPLVk4TEDao"},
+      {"Music Like Dirt (2023), for string orchestra",
+        "https://www.youtube.com/watch?v=AVZ4SV8SuaE"},
       {"Earth and Green (2019, rev. 2023), for piano solo",
        "https://www.youtube.com/watch?v=njsQobL6Q_Y"},
       {"a sensation of order (2022), for Eb clarinet, written for Raymond Brien",
-       "https://youtu.be/g-nqfkkmf3A?si=th5POhukLkCnkqMi&t=867"},
+        "https://youtu.be/g-nqfkkmf3A?si=th5POhukLkCnkqMi&t=867"},
       {"Amuse Bouche (2021), for alto saxophone, written for Stephan Tamas",
-       "https://youtu.be/bq7HA0YY9eM?si=VmwkdSDs1piBQCto&t=2307"},
+        "https://youtu.be/bq7HA0YY9eM?si=VmwkdSDs1piBQCto&t=2307"},
       {"Boum (2020), for soprano voice, written for Stephanie Lamprea",
-       "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"}
+        "https://www.facebook.com/stephanie.lamprea/posts/10215389397038432"}
     ]
   end
 end
